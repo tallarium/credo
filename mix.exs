d88cc9d3e34bb19c2d0a918106b8b10d70e6bae2
Sam Nipps <sam@tallarium.com>
2020-06-26 17:34:42 +0100

    v0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TallariumCredo.Mixfile do
   def project do
     [
       app: :tallarium_credo,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.10.1",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
